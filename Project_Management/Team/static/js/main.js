d65774cae7626b5391667b1c93415fa408d394e5
Lino Domingos Chitlhango <lino.chitlhango@gmail.com>
2024-03-20 01:58:45 +0200

    Team App criado

---
+++ Project_Management/Team/static/js/main.js
@@ -0,0 +1,18 @@
+let arrows = document.querySelectorAll(".arrow");
+for (let i = 0; i < arrows.length; i++) {
+    arrows[i].addEventListener("click", function(e) {
+        let arrowParent = e.target.parentElement.parentElement; // Selecionando o elemento pai do arrow
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+sidebarBtn.addEventListener("click", function() {
+    sidebar.classList.toggle("close");
+});
+
+function logout() {
+    // Redireciona para a página de login
+    window.location.href = "/pages/login.html";
+}
---
+++ Project_Management/Team/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Equipe (request):
+    return render (request, 'team.html')
