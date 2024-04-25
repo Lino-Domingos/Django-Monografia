846358862d3813d1234f81dbf623d37a368397ad
Lino Domingos Chitlhango <lino.chitlhango@gmail.com>
2024-04-25 09:25:22 +0200

    Acoount App: Organizar Templates em um TEMPLATES

--- Project_Management/Accounts/admin.py
+++ Project_Management/Accounts/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Direccao_Regional, ASC, User
 
 
+# Registo das Tabelas ADMIN
 admin.site.register(Direccao_Regional)
 admin.site.register(ASC)
 admin.site.register(User)
